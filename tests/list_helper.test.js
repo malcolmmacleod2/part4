49a05a613583ac1d3b0f06d5f9b7f98a6e076f73
Malcolm Macleod <malcolm@macleodhome.com>
2021-02-07 23:08:20 +0000

    Step 3 - favourite blog

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,8 +1,103 @@
-const listHelper = require('../utils/list_helper')
+const listHelper = require("../utils/list_helper");
 
-test('dummy returns one', () => {
-  const blogs = []
+test("dummy returns one", () => {
+  const blogs = [];
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
-})
+  const result = listHelper.dummy(blogs);
+  expect(result).toBe(1);
+});
+
+describe("total likes", () => {
+  test("when list has only one blog, equals the likes of that", () => {
+    const listWithOneBlog = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url:
+          "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+    ];
+
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  test("totalLikes returns 64", () => {
+    const blogs = [
+      {
+        title: "you",
+        author: "bob",
+        url: "http://allaboutbob.me",
+        likes: 10,
+      },
+      {
+        title: "you",
+        author: "bob",
+        url: "http://allaboutbob.me",
+        likes: 54,
+      },
+      {
+        title: "you",
+        author: "bob",
+        url: "http://allaboutbob.me",
+        likes: 0,
+      },
+    ];
+
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(64);
+  });
+});
+
+describe("Favourite blog", () => {
+  test("One blog returns 1 like", () => {
+    const blogs = [
+      {
+        title: "you",
+        author: "bob",
+        url: "http://allaboutbob.me",
+        likes: 10,
+      },
+    ];
+
+    const result = listHelper.favouriteBlog(blogs);
+    expect(result).toEqual(blogs[0]);
+  });
+
+  test("Zero blog returns empty", () => {
+    const blogs = [];
+
+    const result = listHelper.favouriteBlog(blogs);
+    expect(result).toEqual([]);
+  });
+
+  test("favouriteBlog of 3 returns you2", () => {
+    const blogs = [
+      {
+        title: "you1",
+        author: "bob",
+        url: "http://allaboutbob.me",
+        likes: 10,
+      },
+      {
+        title: "you2",
+        author: "bob",
+        url: "http://allaboutbob.me",
+        likes: 54,
+      },
+      {
+        title: "you3",
+        author: "bob",
+        url: "http://allaboutbob.me",
+        likes: 0,
+      },
+    ];
+
+    const result = listHelper.favouriteBlog(blogs);
+    console.log(result);
+    expect(result.name).toEqual(blogs[1].name);
+  });
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,19 @@
 const dummy = (blogs) => {
   // ...
-  return 1
-}
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.map((b) => b.likes).reduce((s, v) => s + v);
+};
+
+const favouriteBlog = (blogs) => {
+  sorted = blogs.sort((a, b) => b.likes - a.likes);
+  return sorted.length > 0 ? blogs[0] : [];
+};
 
 module.exports = {
-  dummy
-}
+  dummy,
+  totalLikes,
+  favouriteBlog,
+};
