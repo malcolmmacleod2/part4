962c45af3541f1996f9864c3aed91a971bec1f17
Malcolm Macleod <malcolm@macleodhome.com>
2021-02-11 21:55:34 +0000

    4.8 step 1 - more changes to test setup inluding different db for test

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -4,6 +4,38 @@ const app = require("../app");
 
 const api = supertest(app);
 
+const Blog = require("../models/blog");
+const initialBlogs = [
+  {
+    title: "you1",
+    author: "bob",
+    url: "http://allaboutbob.me",
+    likes: 10,
+  },
+  {
+    title: "you2",
+    author: "bob",
+    url: "http://allaboutbob.me",
+    likes: 54,
+  },
+  {
+    title: "you3",
+    author: "bob",
+    url: "http://allaboutbob.me",
+    likes: 0,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[2]);
+  await blogObject.save();
+});
+
 test("blogs are returned as json", async () => {
   await api
     .get("/api/blogs")
