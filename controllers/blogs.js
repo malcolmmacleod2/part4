84be4f23513d9039cb201edd657bb73e80ae8a76
Malcolm Macleod <malcolm@macleodhome.com>
2021-02-14 19:37:40 +0000

    4.19 - blog added by user which logged in

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,16 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user");
 
@@ -9,6 +19,13 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+
   if (!request.body.title) {
     return response.status(400).json({
       error: "title missing",
@@ -21,9 +38,7 @@ blogsRouter.post("/", async (request, response) => {
     });
   }
 
-  const users = await User.find({});
-  const randomIndex = Math.floor(Math.random() * users.length);
-  const user = users[randomIndex];
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog(request.body);
   blog.user = user._id;
